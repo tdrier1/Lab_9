5eb6f857967e47a28535c553690e6c3667112b49
tdrier1 <tdrier1@gmail.com>
2017-04-23 20:15:36 -0400

    Program up and running

---
+++ Lab_9/Circle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab__9
+{
+    class Circle
+    {
+        public double radius;
+        public double diameter;
+        public double circumference;
+        public double area;
+
+
+        public Circle(double radius)
+        {
+            diameter = 0;
+            circumference = 0;
+            area = 0;
+        }
+
+        public static double getCircumference(double diameter)
+        {
+            double circumference = Math.PI * (diameter);
+
+            return circumference;
+        }
+
+        public static string getFormattedCircumference(double circumference)
+        {
+
+            string CircForm = circumference.ToString("0.##");
+
+            return CircForm;
+        }
+
+        public static double getArea(double radius)
+        {
+            double area = Math.PI * (Math.Pow(radius, 2));
+
+            return area;
+        }
+
+        public static string getFormattedArea(double area)
+        {
+
+            string areaForm = area.ToString("0.##");
+
+            return areaForm;
+        }
+    }
+}
---
+++ Lab_9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab__9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Welcome to the Circle Tester");
+            Console.Write("Enter Radius: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            double diameter = 2 * (radius);
+
+            Circle cirCalc = new Circle(radius);
+
+            Console.WriteLine("Circumference: " + Circle.getFormattedCircumference(Circle.getCircumference(diameter)));
+            Console.WriteLine("Area: " + Circle.getFormattedArea(Circle.getArea(radius)));
+
+            Console.WriteLine("Continue? (y/n)");
+
+
+
+        }
+    }
+}

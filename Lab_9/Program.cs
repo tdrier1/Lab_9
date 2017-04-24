c6ed7d77d9bbb0ae24f7ca5ef3fc5d2bedf165fb
tdrier1 <tdrier1@gmail.com>
2017-04-23 20:48:42 -0400

    Finished lab with validation

--- Lab_9/Program.cs
+++ Lab_9/Program.cs
@@ -10,23 +10,85 @@ namespace Lab__9
     {
         static void Main(string[] args)
         {
+            bool run = true;
 
+            List<Circle> CircList = new List<Circle>();//create list to store circle data
 
-            Console.WriteLine("Welcome to the Circle Tester");
-            Console.Write("Enter Radius: ");
-            double radius = Convert.ToDouble(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Welcome to the Circle Tester");
+                Console.WriteLine("++++++++++++++++++++++++++++");
+                Console.Write("\nEnter Radius: ");
+                double radius = GetDbl();//validates data being entered
 
-            double diameter = 2 * (radius);
+                getCalculations(radius, CircList);//method used to call Circle class
 
-            Circle cirCalc = new Circle(radius);
+                run=Continue(CircList);//method used to prompt user to continue
 
-            Console.WriteLine("Circumference: " + Circle.getFormattedCircumference(Circle.getCircumference(diameter)));
-            Console.WriteLine("Area: " + Circle.getFormattedArea(Circle.getArea(radius)));
+            } while (run == true);
+        }
 
-            Console.WriteLine("Continue? (y/n)");
+        public static double GetDbl()
+        {
+            double value;
 
+            while (!double.TryParse(Console.ReadLine(), out value))
+            {
+                {
+                    Console.Write("Try again! Please enter a number(can be decimals): ");
+                }
+            }
+            return value;
+        }//validation for input
 
+        public static void getCalculations(double radius, List<Circle> CircList)
+        {
+            double diameter = 2 * (radius);//performs calc and sends diameter to other class
 
+            Circle cirCalc = new Circle(radius);//creates a new circle object
+
+            CircList.Add(cirCalc);//adds circle obj to list
+
+            Console.WriteLine("\nCircumference: " + Circle.getFormattedCircumference(Circle.getCircumference(diameter)));//prints cicle data to the console by calling methods in other class
+            Console.WriteLine("\nArea: " + Circle.getFormattedArea(Circle.getArea(radius)));
+        }
+
+        public static bool Continue(List<Circle> CircList)//standard loop to prompt user to continue with validation
+        {
+            string conf;
+            bool check = true;
+            bool run = true;
+
+            Console.WriteLine("\n=============================");
+            Console.WriteLine("\nWould you like to play again?");
+            Console.Write("\nPlease Enter (y/n): ");
+
+            do
+            {
+                conf = Console.ReadLine();
+                var t = conf.ToLower();
+
+                if (t != "y" || t != "n")
+                {
+                    Console.Write("\nPlease Enter (y/n): ");
+                }
+                if (t == "y")
+                {
+                    Console.Clear();
+                    check = false;
+                }
+                if (t == "n")
+                {
+                    Console.Clear();
+                    Console.WriteLine("You built {0} circles!", CircList.Count);
+                    Console.WriteLine("\nThank you and goodbye.\n");
+
+                    run = false;
+                    check = false;
+                }
+            } while (check == true);
+
+            return run;
         }
     }
 }
